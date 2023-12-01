b0ff3afa84a200cde2e1356f9769d7add32c41aa
Fabio Di Peri <fabiodiperi@outlook.com>
2023-12-01 18:47:00 +0100

    Day 1 2021

--- src/Year2021/Day1/Day1.cs
+++ src/Year2021/Day1/Day1.cs
@@ -1,20 +1,34 @@
+using Utilities.Extensions;
+
 namespace Year2021;
 
 public class Day1
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 7)]
+    [FileLines("data.txt", 1139)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var increments = data
+            .Select(int.Parse)
+            .SlidingWindow(2)
+            .Count(w => w[0] < w[1]);
+
+        Assert.Equal(result, increments);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 5)]
+    [FileLines("data.txt", 1103)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var increments = data
+            .Select(int.Parse)
+            .SlidingWindow(3)
+            .Select(Enumerable.Sum)
+            .SlidingWindow(2)
+            .Count(w => w[0] < w[1]);
+
+        Assert.Equal(result, increments);
     }
 }
