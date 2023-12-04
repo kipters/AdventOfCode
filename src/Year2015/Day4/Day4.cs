35965d6812f33df23eb691b148e94d2aa109bed8
Fabio Di Peri <fabiodiperi@outlook.com>
2023-12-04 20:59:49 +0100

    Day 4 2015

--- src/Year2015/Day4/Day4.cs
+++ src/Year2015/Day4/Day4.cs
@@ -1,20 +1,39 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Year2015;
 
 public class Day4
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part1(IEnumerable<string> data, int result)
+    [Theory]
+    [InlineData("abcdef", 609043)]
+    [InlineData("pqrstuv", 1048970)]
+    [InlineData("yzbqklnj", 282749)]
+    public void Part1(string data, int result)
     {
-        throw new NotImplementedException();
+        var number = Enumerable.Range(0, int.MaxValue)
+            .First(i =>
+            {
+                var buffer = Encoding.UTF8.GetBytes($"{data}{i}");
+                var hash = Convert.ToHexString(MD5.HashData(buffer));
+                return hash.StartsWith("00000");
+            });
+
+        Assert.Equal(result, number);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part2(IEnumerable<string> data, int result)
+    [Theory]
+    [InlineData("yzbqklnj", 9962624)]
+    public void Part2(string data, int result)
     {
-        throw new NotImplementedException();
+        var number = Enumerable.Range(0, int.MaxValue)
+            .First(i =>
+            {
+                var buffer = Encoding.UTF8.GetBytes($"{data}{i}");
+                var hash = MD5.HashData(buffer);
+                return hash[0] == 0 && hash[1] == 0 && hash[2] == 0;
+            });
+
+        Assert.Equal(result, number);
     }
 }
