68b89a8a962c1206a9643326e9fd59e7e65a1025
Fabio Di Peri <fabiodiperi@outlook.com>
2024-09-14 04:37:10 +0200

    Day 8 2015

--- src/Year2015/Day8/Day8.cs
+++ src/Year2015/Day8/Day8.cs
@@ -1,20 +1,89 @@
+using System.Buffers;
+
 namespace Year2015;
 
 public class Day8
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 12)]
+    [FileLines("data.txt", 1342)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var diff = data
+            .Select(DiffLength)
+            .Sum();
+
+        Assert.Equal(result, diff);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    private static int DiffLength(string line)
+    {
+        var chars = line.Length;
+        var count = 0;
+        var isEscaping = false;
+
+        for (var i = 0; i < chars; i++)
+        {
+            var c = line[i];
+
+            if (char.IsWhiteSpace(c))
+            {
+                continue;
+            }
+
+            if (!isEscaping && c == '\"')
+            {
+                continue;
+            }
+
+            if (!isEscaping && c == '\\')
+            {
+                isEscaping = true;
+                continue;
+            }
+
+            if (isEscaping && c == 'x')
+            {
+                count++;
+                i += 2;
+                isEscaping = false;
+                continue;
+            }
+
+            count++;
+            isEscaping = false;
+        }
+
+        return chars - count;
+    }
+
+    [Theory]
+    [FileLines("data_sample.txt", 19)]
+    [FileLines("data.txt", 2074)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var diff = data
+            .Select(DiffExpansion)
+            .Sum();
+
+        Assert.Equal(result, diff);
+    }
+
+    private static int DiffExpansion(string line)
+    {
+        var chars = line.Length;
+        var count = 2;
+
+        for (var i = 0; i < chars; i++)
+        {
+            var c = line[i];
+
+            if (c is '\"' or '\\')
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
