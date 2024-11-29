4eae274b7d9f6864d31299e47f20f28172896ecd
Fabio Di Peri <fabio@kipters.dev>
2024-11-29 17:33:58 +0100

    Day 3 2016

--- src/Year2016/Day3/Day3.cs
+++ src/Year2016/Day3/Day3.cs
@@ -2,19 +2,43 @@ namespace Year2016;
 
 public class Day3
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data.txt", 862)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var count = data
+            .Select(l => l
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(int.Parse)
+                .ToArray()
+            )
+            .Count(IsValid);
+
+        Assert.Equal(result, count);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data.txt", 1577)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var count = data
+            .Select(l => l
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(int.Parse)
+                .ToArray()
+            )
+            .Chunk(3)
+            .SelectMany(c => new bool[]
+            {
+                IsValid(c[0][0], c[1][0], c[2][0]),
+                IsValid(c[0][1], c[1][1], c[2][1]),
+                IsValid(c[0][2], c[1][2], c[2][2]),
+            })
+            .Count(_ => _);
+
+        Assert.Equal(result, count);
     }
+
+    private static bool IsValid(int[] triangle) => IsValid(triangle[0], triangle[1], triangle[2]);
+    private static bool IsValid(int a, int b, int c) => a + b > c && a + c > b && b + c > a;
 }
