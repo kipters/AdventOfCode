4d98e743d75cb0318b6a0883616c36f39849dd01
Fabio Di Peri <fabiodiperi@outlook.com>
2024-11-30 23:04:13 +0100

    Day 9 Year 2020

--- src/Year2020/Day9/Day9.cs
+++ src/Year2020/Day9/Day9.cs
@@ -1,20 +1,54 @@
+using Utilities.Extensions;
+
 namespace Year2020;
 
 public class Day9
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part1(IEnumerable<string> data, int result)
+    [Theory]
+    [FileLines("data_sample.txt", 5, 127L)]
+    [FileLines("data.txt", 25, 1721308972L)]
+    public void Part1(IEnumerable<string> data, int preambleSize, long result)
     {
-        throw new NotImplementedException();
+        var notSum = data
+            .Select(long.Parse)
+            .SlidingWindow(preambleSize + 1)
+            .First(IsNotSum);
+
+        Assert.Equal(result, notSum[^1]);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part2(IEnumerable<string> data, int result)
+    private bool IsNotSum(long[] arg) => !arg[..^1]
+        .AsPairs()
+        .Select(p => p.a + p.b)
+        .Contains(arg[^1]);
+
+    [Theory]
+    [FileLines("data_sample.txt", 5, 62L)]
+    [FileLines("data.txt", 25, 209694133L)]
+    public void Part2(IEnumerable<string> data, int preambleSize, long result)
     {
-        throw new NotImplementedException();
+        var numData = data
+            .Select(long.Parse)
+            .ToArray();
+
+        var notSum = numData
+            .SlidingWindow(preambleSize + 1)
+            .First(IsNotSum);
+
+        var weakness = Enumerable.Range(2, 1000)
+            .SelectMany(w => FindSumSet(numData, w, notSum[^1]))
+            .Select(s => s.Bounds())
+            .Select(s => s.min + s.max)
+            .First()
+            ;
+
+        Assert.Equal(result, weakness);
+    }
+
+    private static IEnumerable<long[]> FindSumSet(IEnumerable<long> data, int windowSize, long sum)
+    {
+        return data
+            .SlidingWindow(windowSize)
+            .Where(w => w.Sum() == sum);
     }
 }
