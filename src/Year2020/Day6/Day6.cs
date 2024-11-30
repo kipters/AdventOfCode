51332ea8cec9f7a9f0becaca890c3630fa042815
Fabio Di Peri <fabiodiperi@outlook.com>
2024-11-30 20:39:32 +0100

    Day 6 Year 2020

--- src/Year2020/Day6/Day6.cs
+++ src/Year2020/Day6/Day6.cs
@@ -1,20 +1,39 @@
+using Utilities.Extensions;
+
 namespace Year2020;
 
 public class Day6
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 11)]
+    [FileLines("data.txt", 6703)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var answers = data
+            .ChunkBy("")
+            .Select(g => g
+                .SelectMany(_ => _)
+                .Distinct()
+                .Count())
+            .Sum();
+
+        Assert.Equal(result, answers);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 6)]
+    [FileLines("data.txt", 3430)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var answers = data
+            .ChunkBy("")
+            .Select(g => g
+                .SelectMany(_ => _)
+                .GroupBy(_ => _)
+                .Count(gr => gr.Count() == g.Length)
+            )
+            .Sum();
+
+        Assert.Equal(result, answers);
     }
 }
