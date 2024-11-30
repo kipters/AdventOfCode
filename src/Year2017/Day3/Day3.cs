86de8195e4da801b194c8dd12d0142bc11e965a0
Fabio Di Peri <fabiodiperi@outlook.com>
2024-11-30 16:47:30 +0100

    Day 3 Year 2017

--- src/Year2017/Day3/Day3.cs
+++ src/Year2017/Day3/Day3.cs
@@ -1,20 +1,91 @@
+using System.Numerics;
+using Utilities;
+using Utilities.Movement;
+
 namespace Year2017;
 
 public class Day3
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part1(IEnumerable<string> data, int result)
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(12, 3)]
+    [InlineData(23, 2)]
+    [InlineData(1024, 31)]
+    [InlineData(277678, 475)]
+    public void Part1(int data, int result)
     {
-        throw new NotImplementedException();
+        var coordinates = GetSpiralCoordinates(data);
+        var steps = Math.Abs(coordinates.X) + Math.Abs(coordinates.Y);
+
+        Assert.Equal(result, steps);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
-    public void Part2(IEnumerable<string> data, int result)
+    private static Vector2 GetSpiralCoordinates(int index) => WalkSpiral().Skip(index - 1).First();
+
+    private static IEnumerable<Vector2> WalkSpiral()
     {
-        throw new NotImplementedException();
+        var plane = new CartesianPlane(Vector2.Zero, Direction.South);
+
+        yield return plane.Position;
+        var steps = 1;
+
+        for(;;)
+        {
+            plane.Turn(TurnDirection.Left);
+
+            for (var i = 0; i < steps; i++)
+            {
+                plane.Walk(1);
+                yield return plane.Position;
+            }
+
+            plane.Turn(TurnDirection.Left);
+
+            for (var i = 0; i < steps; i++)
+            {
+                plane.Walk(1);
+                yield return plane.Position;
+            }
+
+            steps++;
+        }
+    }
+
+    [Theory]
+    [InlineData(277678, 279138)]
+    public void Part2(int data, int result)
+    {
+        var cells = new Dictionary<Vector2, int>
+        {
+            [Vector2.Zero] = 1
+        };
+
+        var output = 0;
+
+        foreach (var square in WalkSpiral().Skip(1))
+        {
+            var value = 0;
+
+            value += cells.TryGetValue(new(square.X - 1, square.Y + 1), out var v) ? v : 0;
+            value += cells.TryGetValue(new(square.X,     square.Y + 1), out v) ? v : 0;
+            value += cells.TryGetValue(new(square.X + 1, square.Y + 1), out v) ? v : 0;
+
+            value += cells.TryGetValue(new(square.X - 1, square.Y    ), out v) ? v : 0;
+            value += cells.TryGetValue(new(square.X + 1, square.Y    ), out v) ? v : 0;
+
+            value += cells.TryGetValue(new(square.X - 1, square.Y - 1), out v) ? v : 0;
+            value += cells.TryGetValue(new(square.X,     square.Y - 1), out v) ? v : 0;
+            value += cells.TryGetValue(new(square.X + 1, square.Y - 1), out v) ? v : 0;
+
+            if (value > data)
+            {
+                output = value;
+                break;
+            }
+
+            cells[square] = value;
+        }
+
+        Assert.Equal(result, output);
     }
 }
