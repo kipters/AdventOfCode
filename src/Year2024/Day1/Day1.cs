ac506847d86ba1ded3096a0a19faff3ca5aef1c5
Fabio Di Peri <fabiodiperi@outlook.com>
2024-12-01 08:41:14 +0100

    Day 1 Year 2024

--- src/Year2024/Day1/Day1.cs
+++ src/Year2024/Day1/Day1.cs
@@ -2,19 +2,48 @@ namespace Year2024;
 
 public class Day1
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 11)]
+    [FileLines("data.txt", 1651298)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var pairs = data
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            .ToArray();
+
+        var left = pairs
+            .Select(p => int.Parse(p[0]))
+            .Order();
+
+        var right = pairs
+            .Select(p => int.Parse(p[1]))
+            .Order();
+
+        var totalDistance = left.Zip(right)
+            .Sum((t) => Math.Abs(t.First - t.Second));
+
+        Assert.Equal(result, totalDistance);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 31)]
+    [FileLines("data.txt", 21306195)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var pairs = data
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            .ToArray();
+
+        var lut = pairs
+            .Select(p => int.Parse(p[1]))
+            .GroupBy(n => n)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        var score = pairs
+            .Select(p => int.Parse(p[0]))
+            .Select(n => lut.TryGetValue(n, out var count) ? n * count : 0)
+            .Sum();
+
+        Assert.Equal(result, score);
     }
 }
