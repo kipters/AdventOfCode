67e0727d010cc31fdbecb600d311b00d70563709
Fabio Di Peri <fabiodiperi@outlook.com>
2024-12-04 00:21:26 +0100

    Day 2 2021

--- src/Year2021/Day2/Day2.cs
+++ src/Year2021/Day2/Day2.cs
@@ -2,19 +2,45 @@ namespace Year2021;
 
 public class Day2
 {
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 150)]
+    [FileLines("data.txt", 1762050)]
     public void Part1(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var depth = data
+            .Select(l => l.Split(' '))
+            .Select(g => (cmd: g[0], arg: int.Parse(g[1])))
+            .Aggregate((depth: 0, position: 0), (a, t) => t switch
+            {
+                ("forward", int v) => a with { position = a.position + v },
+                ("down", int v) => a with { depth = a.depth + v },
+                ("up", int v) => a with { depth = a.depth - v },
+                _ => throw new ArgumentException($"Invalid command {t.cmd}")
+            }, a => a.depth * a.position);
+
+        Assert.Equal(result, depth);
     }
 
-    //[Theory]
-    //[FileLines("data_sample.txt", 0)]
-    //[FileLines("data.txt", 0)]
+    [Theory]
+    [FileLines("data_sample.txt", 900)]
+    [FileLines("data.txt", 1855892637)]
     public void Part2(IEnumerable<string> data, int result)
     {
-        throw new NotImplementedException();
+        var depth = data
+            .Select(l => l.Split(' '))
+            .Select(g => (cmd: g[0], arg: int.Parse(g[1])))
+            .Aggregate((depth: 0, position: 0, aim: 0), (a, t) => t switch
+            {
+                ("forward", int v) => a with
+                {
+                    position = a.position + v,
+                    depth = a.depth + v * a.aim
+                },
+                ("down", int v) => a with { aim = a.aim + v },
+                ("up", int v) => a with { aim = a.aim - v },
+                _ => throw new ArgumentException($"Invalid command {t.cmd}")
+            }, a => a.depth * a.position);
+
+        Assert.Equal(result, depth);
     }
 }
