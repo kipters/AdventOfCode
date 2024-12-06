e6e5d919068b5ed91521c3f805435b9e36a63bab
Fabio Di Peri <fabio@kipters.dev>
2024-12-06 13:50:33 +0100

    Fix inverted direction vectors in utilities

--- src/Utilities/Movement/Directions.cs
+++ src/Utilities/Movement/Directions.cs
@@ -4,9 +4,9 @@ namespace Utilities;
 
 internal static class DirectionVectors
 {
-    internal static readonly Vector2 North = new(0, +1);
+    internal static readonly Vector2 North = new(0, -1);
     internal static readonly Vector2 East = new(1, 0);
-    internal static readonly Vector2 South = new(0, -1);
+    internal static readonly Vector2 South = new(0, 1);
     internal static readonly Vector2 West = new(-1, 0);
 
     internal static readonly Direction[] Compass = [
